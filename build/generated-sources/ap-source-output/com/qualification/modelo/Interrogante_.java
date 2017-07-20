7192a680848d5fd0c12e7d49a60b76a0492a5aaf
Jorge Manuel Alvarez <alvarezmolina2398@gmail.com>
2017-07-20 12:25:24 -0600

    agregado de librerias a folder y configuracion de struct2

--- build/generated-sources/ap-source-output/com/qualification/modelo/Interrogante_.java
+++ build/generated-sources/ap-source-output/com/qualification/modelo/Interrogante_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-07-20T09:56:08")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-07-20T12:22:42")
 @StaticMetamodel(Interrogante.class)
 public class Interrogante_ { 
 
